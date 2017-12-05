8204b83a3d29be887fb85a015f7cc0c491ed54f3
caleb kandoro <kandoroc@hotmail.com>
2017-12-05 11:20:44 +0200

    deleted migrations

--- common_base/admin.py
+++ common_base/admin.py
@@ -4,6 +4,4 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Account
 
-admin.site.register(Account)
-
-# Register your models here.
+#admin.site.register(Account)
